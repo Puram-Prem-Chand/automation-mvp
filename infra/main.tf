0b935aa40d86e5c4c7db389226f2c6dbed3798e2
Puram-Prem-Chand <purampremchandnaidu@gmail.com>
2026-02-27 20:44:05 +0500

    Update Terraform configuration

--- infra/main.tf
+++ infra/main.tf
@@ -15,10 +15,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Find latest Ubuntu 22.04 LTS AMI for the region
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical
+  owners      = ["099720109477"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
@@ -31,24 +30,22 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH from your IP and HTTP from anywhere"
+  name = "web-sg"
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    # Set this to your IP/32 for safety (pass via Terraform var or edit after apply)
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -69,7 +66,6 @@ resource "aws_instance" "web" {
   }
 }
 
-# Write a tiny text file locally with the instance IP (useful)
 resource "local_file" "ansible_inventory" {
   content  = "web ansible_host=${aws_instance.web.public_ip} ansible_user=ubuntu\n"
   filename = "${path.cwd}/../ansible/inventory.ini"
